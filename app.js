1c92d4d4fe7c3674d653ec4088dce554c71237c9
Laurent Brugière de Barante <laurentbrugiere@yahoo.fr>
2024-02-29 17:34:52 +1100

    added express-rate-limit middleware

--- app.js
+++ app.js
@@ -36,6 +36,14 @@ app.use(
     },
   }),
 );
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
